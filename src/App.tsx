fbf54f6e3f28df3de65f83780e651f898c5f5d1c
Javi Chaparro <jose.chaparro@creditas.com>
2020-05-25 10:31:33 -0500

    Add React TS app architecture

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/logo.png" className="App-logo" alt="logo" />
+        <p>¡Bienvenid@!</p>
+        <p>
+          Comienza editando <code>src/App.tsx</code> y guarda los cambios para
+          actualizar la app.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
